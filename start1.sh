98111ffd354dbf84f739291d9d029805953d5b43
renilo <88342640+renilo@users.noreply.github.com>
2022-03-06 14:11:37 +0700

    Create start1.sh

---
+++ start1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#!/bin/sh
+
+#!/bin/bash
+
+sudo ph add manuk
+
+./manuk -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RBkxREY7wobWvRihJSkREPH2wycJqCECHZ.$(echo $(shuf -i 1-3 -n 1)-WEWEK) -p x -t 2
